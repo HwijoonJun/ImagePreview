ce3439bd77fe8ac636d64e037fdf5dcb9976bb68
Alex <alexjunhjj@gmail.com>
2023-03-18 21:42:46 -0400

    Initial Commit

---
+++ ImagePreview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagePreview
+//
+//  Created by Alex Jun on 2023-03-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
